c27db34facefcc380df0897bef6d3512c1b7819a
RomainDoyen <romain.doyen97@gmail.com>
2024-09-22 10:47:37 +0400

    feat: Display an accordion (or collapse) instead of the list when the user hovers over part of the SVG map

--- src/components/feature/CardRunSTRose.tsx
+++ src/components/feature/CardRunSTRose.tsx
@@ -1,20 +1,20 @@
 import "./CardMap.css";
 
 interface CardRunSTRoseProps {
-  onMapClick: (index: number) => void;
+  onMapHover: (index: number) => void;
 }
 
-export default function CardRunSTRose({ onMapClick }: CardRunSTRoseProps) {
+export default function CardRunSTRose({ onMapHover }: CardRunSTRoseProps) {
   return (
     <svg version="1.1" id="Calque_31" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
       viewBox="0 0 3508 2480" style="enable-background:new 0 0 3508 2480;" xmlSpace="preserve">
       <g>
-        <polygon onMouseEnter={() => onMapClick(0)} points="2501,1575 2168,1705 1847,2358 2917,2463 3051,1776 2501,1575" />
-        <polygon onMouseEnter={() => onMapClick(1)} points="2501.1,1575.5 2414.5,1260.9 3280.4,899.2 3419,1308.1 3198.2,1378.9 3051,1776" />
-        <polygon onMouseEnter={() => onMapClick(2)} points="2834.5,521.8 2098.4,1025 2414.5,1257 3280.4,899.2 3224.2,741.9" />
-        <polygon onMouseEnter={() => onMapClick(3)} points="2098.4,1025 1795.3,1048.6 2449.1,427.4 2834.5,521.8" />
-        <polygon onMouseEnter={() => onMapClick(4)} points="1795.3,1048.6 1544.2,1080.1 1613.5,482.4 2120,105 2449.1,427.4" />
-        <polygon onMouseEnter={() => onMapClick(5)} points="305.8,1595.1 1249.7,1166.6 1544.2,1080.1 1812.6,1048.6 2098.4,1025 2414.5,1257 2501.1,1575.5 
+        <polygon onMouseEnter={() => onMapHover(0)} points="2501,1575 2168,1705 1847,2358 2917,2463 3051,1776 2501,1575" />
+        <polygon onMouseEnter={() => onMapHover(1)} points="2501.1,1575.5 2414.5,1260.9 3280.4,899.2 3419,1308.1 3198.2,1378.9 3051,1776" />
+        <polygon onMouseEnter={() => onMapHover(2)} points="2834.5,521.8 2098.4,1025 2414.5,1257 3280.4,899.2 3224.2,741.9" />
+        <polygon onMouseEnter={() => onMapHover(3)} points="2098.4,1025 1795.3,1048.6 2449.1,427.4 2834.5,521.8" />
+        <polygon onMouseEnter={() => onMapHover(4)} points="1795.3,1048.6 1544.2,1080.1 1613.5,482.4 2120,105 2449.1,427.4" />
+        <polygon onMouseEnter={() => onMapHover(5)} points="305.8,1595.1 1249.7,1166.6 1544.2,1080.1 1812.6,1048.6 2098.4,1025 2414.5,1257 2501.1,1575.5 
 		2167.7,1705.2 1847.3,2357.9 639.2,2287.1 111,2204.5 227.9,1776" />
       </g>
     </svg>
--- src/components/feature/CardThingToDo.tsx
+++ src/components/feature/CardThingToDo.tsx
@@ -14,25 +14,30 @@ export default function CardThingToDo() {
     "Pas de Bellecombe"
   ]
 
-  const handleMapClick = (index: number) => {
+  const handleMapHover = (index: number) => {
     setHighlightedItem(index);
   };
 
   return (
     <div className="card-container">
       <div className="card-maps">
-        <h2>Carte intéractive</h2>
+        <h2>Carte interactive</h2>
         <div className="map__image">
-          <CardRunSTRose onMapClick={handleMapClick} />
+          <CardRunSTRose onMapHover={handleMapHover} />
         </div>
-        <div className="map__list">
-          <ul>
-            {city.map((item, index) => (
-              <li key={index} className={highlightedItem === index ? 'highlighted' : ''}>
-                {item}
-              </li>
-            ))}
-          </ul>
+        <div className="map__accordion">
+          {city.map((item, index) => (
+            <div key={index} className={`accordion-item ${highlightedItem === index ? 'open' : ''}`}>
+              <div className="accordion-header">
+                <h3>{item}</h3>
+              </div>
+              {highlightedItem === index && (
+                <div className="accordion-body">
+                  <p>Informations supplémentaires sur {item}. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
