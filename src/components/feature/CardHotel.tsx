5c4d51e0fbe9aff9a1cca95cc1764553ee212300
RomainDoyen <romain.doyen97@gmail.com>
2024-09-26 10:25:05 +0400

    style: Graphic redesign of the hotel card

--- src/components/feature/CardHotel.tsx
+++ src/components/feature/CardHotel.tsx
@@ -1,23 +1,43 @@
 import data from '../../data/data.json';
-import './CardHotel.css';
 import Image from '../ui/Image';
+import { Bookmark } from 'lucide-react';
+import { Hotel } from 'lucide-react';
+import './CardHotel.css';
 
 export default function CardHotel() {
   return (
     <div className="card-container">
       {data.hotel.map((hotel, index) => (
-        <div className="card" key={index}>
-          <Image 
-            src={hotel.imageUrl} 
-            alt={hotel.name} 
-          />
-          <div className="card-content">
-            <h1>{hotel.name}</h1>
-            <p>{hotel.description}</p>
-            <p>{hotel.address}</p>
-            <p>{hotel.phone}</p>
-          </div>
-        </div>
+        <article class="card" key={index}>
+              <section class="card__hero">
+              <div class="card__hero_head">
+                <span>$150/hr</span>
+                <div class="card__icon">
+                  <Bookmark />
+                </div>
+              </div>
+              <Image 
+                  src={hotel.imageUrl} 
+                  alt={hotel.name} 
+                  className="card__image"
+                />
+              </section>
+
+              <footer class="card__footer">
+                <div class="card__job_summary">
+                  <div class="card__job_icon">
+                    <Hotel />
+                  </div>
+                  <div class="card__job">
+                    <p class="card__job_title">
+                      {hotel.name}
+                    </p>
+                  </div>
+                </div>
+
+                <button class="card__btn">voir</button>
+              </footer>
+        </article>
       ))}
     </div>
   )
