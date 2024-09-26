d890293ea65b6cbdb73be5f905821cb3dcba03db
RomainDoyen <romain.doyen97@gmail.com>
2024-09-26 19:05:24 +0400

    style: Graphic redesign of the CardHistory

--- src/components/feature/CardHistory.tsx
+++ src/components/feature/CardHistory.tsx
@@ -1,13 +1,21 @@
 import data from '../../data/data.json';
+import Image from '../ui/Image';
+import './CardHistory.css';
 
 export default function CardHistory() {
   return (
-    <div>
+    <div className="card-container">
       {data.history.map((history, index) => (
-        <div key={index}>
-          <h1>{history.title}</h1>
-          <p>{history.description}</p>
-          <img src={history.description_photo} alt="" />
+        <div key={index} className="history-card">
+          <h1 className="history-title">{history?.title}</h1>
+          {history.description.split('\n').map((line, lineIndex) => (
+            <p className="history-description" key={lineIndex}>{line}</p>
+          ))}
+          <Image 
+            src={history?.description_photo} 
+            alt={history?.title} 
+            className="history-image" 
+          />
         </div>
       ))}
     </div>
