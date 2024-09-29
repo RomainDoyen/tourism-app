d212465a683731d162e6d467b489ca84451eb2d3
RomainDoyen <romain.doyen97@gmail.com>
2024-09-29 20:45:02 +0400

    refactor: test for images not displayed

--- src/data/places.ts
+++ src/data/places.ts
@@ -4,35 +4,42 @@ type Place = {
   image: string;
 };
 
+import forest from "../assets/forest.jpg";
+import cascade from "../assets/cascade.jpg";
+import ravine from "../assets/ravine.jpg";
+import mairie from "../assets/mairie.jpg";
+import eglise from "../assets/eglise.jpg";
+import pasdebellecombe from "../assets/pasdebellecombe.jpg";
+
 export const places: Place[] = [
   {
     name: "Forêt de Bois-Blanc",
     description: "Une magnifique forêt située à proximité du village.",
-    image: "./src/assets/forest.jpg",
+    image: forest,
   },
   {
     name: "Anse des Cascades",
     description: "Un lieu incontournable avec des cascades magnifiques.",
-    image: "./src/assets/cascade.jpg",
+    image: cascade,
   },
   {
     name: "Ravine Glissante",
     description: "Un site naturel offrant des randonnées spectaculaires.",
-    image: "./src/assets/ravine.jpg",
+    image: ravine,
   },
   {
     name: "Mairie de Sainte Rose",
     description: "Le bâtiment administratif central de la ville.",
-    image: "./src/assets/mairie.jpg",
+    image: mairie,
   },
   {
     name: "Eglise Notre Dame des Laves",
     description: "Une église emblématique qui a survécu aux coulées de lave.",
-    image: "./src/assets/eglise.jpg",
+    image: eglise,
   },
   {
     name: "Pas de Bellecombe",
     description: "Un point de vue exceptionnel sur le volcan.",
-    image: "./src/assets/pasdebellecombe.jpg",
+    image: pasdebellecombe,
   },
 ];
