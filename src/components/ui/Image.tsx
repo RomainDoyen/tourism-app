3e50138dd4a185b48d8d254a773b21d0316ab94b
RomainDoyen <romain.doyen97@gmail.com>
2024-09-29 20:03:27 +0400

    refactor: test for images not displayed

--- src/components/ui/Image.tsx
+++ src/components/ui/Image.tsx
@@ -1,20 +1,42 @@
 import { ImageProps } from "../../types/ui_types"
-import defaultImage from '../../assets/default-image.jpg';
-import { useState } from 'preact/hooks';
+// import defaultImage from '../../assets/default-image.jpg';
+// import { useState } from 'preact/hooks';
 
 export default function Image({ src, alt, className }: ImageProps) {
-  const [imgSrc, setImgSrc] = useState(src || defaultImage);
+  // const [imgSrc, setImgSrc] = useState(src || defaultImage);
 
-  const handleError = () => {
-    setImgSrc(defaultImage);
-  };
+  // const handleError = () => {
+  //   setImgSrc(defaultImage);
+  // };
 
   return (
     <img 
-      src={imgSrc} 
+      src={src} 
       alt={alt} 
       className={className}
-      onError={handleError}
+      // onError={handleError}
     />
   )
 }
+
+
+// import { ImageProps } from "../../types/ui_types"
+// import defaultImage from '../../assets/default-image.jpg';
+// import { useState } from 'preact/hooks';
+
+// export default function Image({ src, alt, className }: ImageProps) {
+//   const [imgSrc, setImgSrc] = useState(src || defaultImage);
+
+//   const handleError = () => {
+//     setImgSrc(defaultImage);
+//   };
+
+//   return (
+//     <img 
+//       src={imgSrc} 
+//       alt={alt} 
+//       className={className}
+//       onError={handleError}
+//     />
+//   )
+// }
