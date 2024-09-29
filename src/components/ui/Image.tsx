aee0bf4d3a23c4bb30aec8ff50c473a97a7cf2eb
RomainDoyen <romain.doyen97@gmail.com>
2024-09-29 21:13:52 +0400

    refactor: test for images not displayed

--- src/components/ui/Image.tsx
+++ src/components/ui/Image.tsx
@@ -1,20 +1,20 @@
 import { ImageProps } from "../../types/ui_types"
-// import defaultImage from '../../assets/default-image.jpg';
-// import { useState } from 'preact/hooks';
+import defaultImage from '../../assets/default-image.jpg';
+import { useState } from 'preact/hooks';
 
 export default function Image({ src, alt, className }: ImageProps) {
-  // const [imgSrc, setImgSrc] = useState(src || defaultImage);
+  const [imgSrc, setImgSrc] = useState(src || defaultImage);
 
-  // const handleError = () => {
-  //   setImgSrc(defaultImage);
-  // };
+  const handleError = () => {
+    setImgSrc(defaultImage);
+  };
 
   return (
     <img 
-      src={src} 
+      src={imgSrc} 
       alt={alt} 
       className={className}
-      // onError={handleError}
+      onError={handleError}
     />
   )
 }
